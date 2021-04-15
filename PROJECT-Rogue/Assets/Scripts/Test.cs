d2c55990c1362633e4743bbb894fe07f4b040060
marset23 <markus23000@gmail.com>
2021-04-15 16:27:49 +0200

    ass

--- PROJECT-Rogue/Assets/Scripts/Test.cs
+++ PROJECT-Rogue/Assets/Scripts/Test.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Console.WriteLine("dupa");
     }
 }
