eaefb23b1d5abc233c8680f6d7dbc46ad329647c
Patryk PC <Patryk.Zub@interia.pl>
2021-04-15 16:25:45 +0200

    test :)

--- PROJECT-Rogue/Assets/Scripts/Test.cs
+++ PROJECT-Rogue/Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        System.Console.WriteLine();
     }
 
     // Update is called once per frame
