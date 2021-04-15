f355ee004413ae0f1bfb53e542bd07c82b7ce4b5
Karol Wydrzyński <slayerqs99@gmail.com>
2021-04-15 16:44:53 +0200

    nie dziala i nie ma tego za dużo

--- PROJECT-Rogue/Assets/Scripts/Test.cs
+++ PROJECT-Rogue/Assets/Scripts/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        System.Console.WriteLine();
+        
     }
 
     // Update is called once per frame
