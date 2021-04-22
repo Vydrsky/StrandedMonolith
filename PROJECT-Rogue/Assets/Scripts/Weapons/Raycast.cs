d1b74e00aa478a92d4c592593ed03992576fcd05
Patryk Zub PC <Patryk.Zub@interia.pl>
2021-04-22 16:30:43 +0200

    Pociski biorą pod uwage prędkość gracza, statystyki broni

--- PROJECT-Rogue/Assets/Scripts/Weapons/Raycast.cs
+++ PROJECT-Rogue/Assets/Scripts/Weapons/Raycast.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Raycast : Ranged
+public abstract class Raycast // : Ranged
 {
     // JESZCZE NIE WIEM CO TU BEDZIE BO NIE WIEM JAK SIE ROBI RAYCAST, ZOBACZYMY
 }
--- PROJECT-Rogue/Assets/Scripts/Weapons/Weapon.cs
+++ PROJECT-Rogue/Assets/Scripts/Weapons/Weapon.cs
@@ -6,24 +6,28 @@ public abstract class Weapon
 {
     protected FightingCharacter whoAttacks;
     protected float attackSpeed;
-    protected int damage;
+    protected float damage;
     float delay; // delay = (1 / attackSpeed)
     float lastShot;
     public abstract void Attack();
 
+    // do usuniecia
     public void SetAttacker(FightingCharacter whoAttacks)
     {
-        this.whoAttacks = whoAttacks;
+        this.whoAttacks = whoAttacks; 
     }
 
 
-    public Weapon()
+    public Weapon(float attackSpeed, float damage)
     {
+        this.attackSpeed = attackSpeed;
+        this.damage = damage;
         lastShot = 0;
         delay = 0.5f;
     }
     public void CheckAttack()
     {
+        delay = (1 / (attackSpeed + whoAttacks.AttackSpeed));
         if (Time.time > lastShot + delay)
         {
             Attack();
